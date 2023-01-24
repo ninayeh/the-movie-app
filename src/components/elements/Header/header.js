8b3445f24b27cad6223069a222844168529ba7f3
ninayeh <itsninayeh@gmail.com>
2023-01-24 15:59:19 +0900

    Update header style and remove some style in App.css

--- src/components/elements/Header/header.js
+++ src/components/elements/Header/header.js
@@ -1,10 +1,19 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
 function Header() {
   return (
-    <div>
-      <header>
-        <p>This is header</p>
-      </header>
-    </div>
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Popular Movie Libriary</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Data sourece <a href="#login">HIHIHIHI</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
